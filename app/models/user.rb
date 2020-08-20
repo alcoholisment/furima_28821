7d813059ba87dbdc1791e88e0572e0bee842d09f
alcoholisment <alcoholism1025@gmail.com>
2020-08-20 18:03:53 +0900

    validates :passwordの正規表現を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :password, format: { with: /[a-z\d{6}]/i}
+  validates :password, format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]+\z/i}
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
     validates :first_name
