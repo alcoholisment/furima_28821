4c2aabd9fd3900586ff53679c2ad68721fde2f00
alcoholisment <alcoholism1025@gmail.com>
2020-08-21 11:17:26 +0900

    苗字名前と苗字名前カナの正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         regEx = /\A[ぁ-んァ-ン一-龥]/
+         regEx1 = /\A[ァ-ヶー－]+\z/
+
   validates :name, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+  with_options presence: true, format: { with: /#{regEx}/ } do
     validates :first_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+  with_options presence: true, format: { with: /#{regEx}/ } do
     validates :first_name_kana
     validates :last_name_kana
   end
