459bef8f61066b3e4a87d566e2c3b8830e2df304
alcoholisment <alcoholism1025@gmail.com>
2020-08-28 19:57:08 +0900

    商品削除に失敗した場合商品のページに戻るように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,8 +16,11 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to items_path
+    if item.destroy
+      redirect_to items_path
+    else
+      redirect_to item_path
+    end
   end
 
   def update
