2c48e06920594f9db618ed09a3d5781c3709dec2
alcoholisment <alcoholism1025@gmail.com>
2020-08-28 19:23:27 +0900

    商品削除機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,12 @@ class ItemsController < ApplicationController
   def edit
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to items_path
+  end
+
   def update
     if @item.update(item_params)
       redirect_to item_path
