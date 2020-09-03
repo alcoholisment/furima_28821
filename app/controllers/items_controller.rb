4625068cbcfef3dcb4cd60fe8945281b8858b367
alcoholisment <alcoholism1025@gmail.com>
2020-09-03 19:02:41 +0900

    出品者のみが編集ページにいけるようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,14 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+    if user_signed_in?
+      if current_user.id != @item.user.id
+        redirect_to root_path
+      end
+    end
   end
 
   def destroy
